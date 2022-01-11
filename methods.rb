feec852acecbb166b8edffcf5d92b902c780cbf4
Ameer <Amir.abh1996@gmail.com>
2022-01-11 11:46:07 -0500

    commit tree de8999a90cf87d7324fab12acfa3264a4b7af415

--- methods.rb
+++ methods.rb
@@ -1,1 +1,11 @@
 # Your code here!
+def greet_programmer 
+   puts "Hello, programmer!"
+end
+def greet name="Naureen"
+    puts "Hello, #{name}!"
+end
+def greet_with_default name="Naureen"
+    puts "Hello, #{name}!"
+    puts "Hello, Programmer!"
+end
